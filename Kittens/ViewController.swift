1ea40eca3110442036e22dc4358f38687b5cf097
kN3TT3R <knetter@ptr-916yrbl8jp4st6yb1qx.18120a2.ip6.access.telenet.be>
2016-12-31 22:42:54 +0100

    Initial Commit

--- Kittens/ViewController.swift
+++ Kittens/ViewController.swift
@@ -10,16 +10,87 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    // MARK: - IBOutlets
+    @IBOutlet weak var addKittenTF: UITextField!
+    @IBOutlet weak var myKittensTV: UITextView!
+    @IBOutlet weak var removeKittenB: UIButton!
+
+
+    // MARK: - Global variables & Constants
+    var myKittens = [String]()
+    let kittensAvailableTitle = "Make a pal happy with the eldest kitten"
+    let noKittensAvailableTitle = "No kittens available"
+    
+    
+    // MARK: - Overridden Functions
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        myKittensTV.text = "My kittens: \n"
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
+    
+    // MARK: - IBActions
+    @IBAction func addKittenButton(_ sender: UIButton) {
+        if !(addKittenTF.text?.isEmpty)! {
+            enableRemoveKittenB()
+            updateMyKittens(sender: sender)
+            updateMyKittensTextView()
+            emptyAddKittenTF()
+        }
+    }
+    
+    @IBAction func removeKittenButton(_ sender: UIButton) {
+        updateMyKittens(sender: sender)
+        updateMyKittensTextView()
+    }
+    
+    
+    // MARK: - Homemade Functions
+    func updateMyKittens(sender: UIButton) {
+        if sender.tag == 1 {
+            myKittens.append(addKittenTF.text!)
+        }
+        if sender.tag == 2 {
+            myKittens.removeFirst()
+        }
+    }
+    
+    func updateMyKittensTextView() {
+        myKittensTV.text = "My kittens: \n"
+        for myKitten in myKittens {
+            myKittensTV.text.append(myKitten)
+            myKittensTV.text.append("\n")
+        }
+        renameRemoveKittenB()
+    }
+    
+    func renameRemoveKittenB() {
+        if myKittens.isEmpty {
+            removeKittenB.setTitle(noKittensAvailableTitle, for: .normal)
+            disableRemoveKittenB()
+        }
+        else {
+            removeKittenB.setTitle(kittensAvailableTitle, for: .normal)
+        }
+    }
+    
+    func emptyAddKittenTF() {
+        addKittenTF.text = ""
+        addKittenTF.resignFirstResponder()
+    }
+    
+    func enableRemoveKittenB() {
+        removeKittenB.isEnabled = true
+    }
+    
+    func disableRemoveKittenB() {
+        removeKittenB.isEnabled = false
+    }
 
 }
 
